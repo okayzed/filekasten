f33a8c1012e439bd957e516ebf06c8614b1c02b8
okay <okay@chalk>
2019-12-23 06:40:31 -0800

    [darkmode] date binary is located via `which`

--- filekasten/dateparse.py
+++ filekasten/dateparse.py
@@ -1,6 +1,7 @@
 import subprocess
 
+date_bin = subprocess.check_output(['which', 'date']).strip()
 def parse_time(s):
-    args = ['/usr/bin/date', '--date=%s' % s, '--', '+%s']
+    args = [date_bin, '--date=%s' % s, '--', '+%s']
     t = subprocess.check_output(args)
     return int(t)
